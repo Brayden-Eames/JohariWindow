1a36bc06288b866c26e105d897b659c2f0cb13b3
Brayden Eames <braydeneames@ymail.com>
2021-09-29 15:16:05 -0600

    Friend Page Fully Functional, both tables populating correctly! QOL changes to ClientConfirmationPage in prep for same page for Friend.

--- JohariWindow/Pages/FriendPage/FriendPage.cshtml.cs
+++ JohariWindow/Pages/FriendPage/FriendPage.cshtml.cs
@@ -16,7 +16,6 @@ namespace JohariWindow.Pages.FriendPage
 
         public AdjectiveModel(IUnitOfWork unitofWork) => _unitofWork = unitofWork;
 
-
         [BindProperty]
         public FriendPageVM FriendObject { get; set; }
 
@@ -27,12 +26,12 @@ namespace JohariWindow.Pages.FriendPage
         public IActionResult OnGet()
         {
             var listOfAdjectives = _unitofWork.Adjective.List();
-
-
             FriendObject = new FriendPageVM()
             {
                 Friend = new ApplicationCore.Models.Friend(),
                 ListOfAdjectives = listOfAdjectives,
+                HowLong = 0,
+                Relationship = "",
                 listOfSelectedAdjectives = new SelectedAdjectiveList()
                 {
                     PositiveAdjectives = new string[12],
@@ -42,24 +41,16 @@ namespace JohariWindow.Pages.FriendPage
             return Page();
         }
 
-        public IActionResult OnPost(string? clientID, string? relationship, int howLong) //need to pass in clientId from the text input field on the Friend page to OnPost()
+        public async Task<IActionResult> OnPost(string? clientID, string? relationship, int howLong) 
         {
-            //first thing we need to do is add the responses (relationship and tenure #s) to the 'Friend' table. 
-            //then submit the adjective choices (also using the ClientId provided in text field) to the FriendResponse Table. 
-
-            var clientId = clientID;
-            var friendRelationship = relationship;
-            var friendHowLong = howLong;
+            var clientId = FriendObject.ClientId;
+            var friendRelationship = FriendObject.Relationship;
+            var friendHowLong = FriendObject.HowLong;
 
             if (!ModelState.IsValid)
             {
                 return Page();
             }
-            if (String.IsNullOrEmpty(clientId))
-            {
-                return NotFound();
-            }
-
             var clientUser = _unitofWork.Client.Get(c=> c.Id == clientId);
 
             _unitofWork.Friend.Add(new ApplicationCore.Models.Friend() //creates the 'friend' and their relationship info in the Friend table. 
@@ -67,20 +58,20 @@ namespace JohariWindow.Pages.FriendPage
                 Relationship = friendRelationship,
                 howLong = friendHowLong
             });
-            _unitofWork.Commit(); //commits the stuff to the Friend Table before doing the FriendResponse table insert. 
+            await _unitofWork.CommitAsync(); //commits the stuff to the Friend Table before doing the FriendResponse table insert. 
+
+            var storedFriendId = _unitofWork.Friend.Get(f => f.Relationship == friendRelationship && f.howLong == friendHowLong);
 
             foreach (string id in Adjectives)
             {
                 _unitofWork.FriendResponse.Add(new ApplicationCore.Models.FriendResponse()
                 {
                     Adjective = _unitofWork.Adjective.Get(a => a.AdjectiveId == int.Parse(id)),
-                    Client = clientUser, 
-                    Friend = _unitofWork.Friend.Get(f => f.FriendId == int.Parse(id))
+                    Client = clientUser,
+                    Friend = _unitofWork.Friend.Get(f => f.FriendId == storedFriendId.FriendId)
                 });
             }
-
             _unitofWork.Commit();
-
             return RedirectToPage("./ClientConfirmationPage");
         }
     }
