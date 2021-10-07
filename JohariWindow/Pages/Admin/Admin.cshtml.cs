17dd6b07c6e3b59282119c3c213122421739217f
Brayden Eames <braydeneames@ymail.com>
2021-10-07 15:10:07 -0600

    Admin Page dropdown with client names populating properly. AdminPageVM , OnGet and HTML page implemented. Next up: Queries in LINQ

--- JohariWindow/Pages/Admin/Admin.cshtml.cs
+++ JohariWindow/Pages/Admin/Admin.cshtml.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApplicationCore.Interfaces;
+using JohariWindow.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +11,29 @@ namespace JohariWindow.Pages.Admin
 {
     public class AdminModel : PageModel
     {
+
+        private readonly IUnitOfWork _unitofWork;
+        public AdminModel(IUnitOfWork unitofWork) => _unitofWork = unitofWork;
+        [BindProperty]
+        public AdminPageVM AdminPageObject { get; set; }
         public void OnGet()
         {
+            IEnumerable<ApplicationCore.Models.Client> clients = _unitofWork.Client.List();
+
+            AdminPageObject = new AdminPageVM()
+            {
+                ClientListData = new List<ClientsListData>(),
+                selectedClientId = ""
+            };
+
+            foreach (ApplicationCore.Models.Client client in clients)
+            {
+                AdminPageObject.ClientListData.Add(new ClientsListData()
+                {
+                    ClientFullName = client.FirstName + " " + client.LastName,
+                    ClientId = client.Id
+                });
+            }
         }
     }
 }
---
+++ JohariWindow/ViewModels/AdminPageVM.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JohariWindow.ViewModels
+{
+    public class AdminPageVM
+    {
+        public Client Client { get; set; }
+        public List<ClientsListData> ClientListData { get; set; }
+        public string selectedClientId { get; set; }
+        public List<AdjectiveDataStruct> OpenSelfSlot { get; set; }
+        public List<AdjectiveDataStruct> BlindSelfSlot { get; set; }
+        public List<AdjectiveDataStruct> HiddenSelfSlot { get; set; }
+        public List<AdjectiveDataStruct> UnknownSelfSlot { get; set; }
+    }
+
+    public struct ClientsListData
+    {
+        public string ClientFullName { get; set; }
+        public string ClientId { get; set; }
+    }
+
+    public struct AdjectiveDataStruct
+    {
+        public string Adjective { get; set; }
+        public int chosenCount { get; set; }
+    }
+}
